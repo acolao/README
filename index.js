c7e47436dae863ad78a9d34ac4b0cee4c50932c7
Alison Colao <alison.siwek@gmail.com>
2023-06-14 22:21:01 -0700

    added questions and consts for README Gen

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'description',
+    message: 'What is a description of your repository?',
+  },  
+  {
+    type: 'input',
+    name: 'table of contents',
+    message: 'What is your table of contents?',
+  }, 
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are your instructions for installation?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How to use this?',
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'What is your license?',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'What is your cosntribution?',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'What are your tests?',
+  },
+  {
+    type: 'input',
+    name: 'questions',
+    message: 'What are your questions?',
+  },
+];
+//title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
